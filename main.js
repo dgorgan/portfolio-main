da5a1b7b03bfb24b6299a2b170ba33892ccf5617
Dumitru Gorgan <dgorgan3@gmail.com>
2016-02-04 21:49:59 -0500

    Major commit: added new images for portfolio & included github/project links, improved site responsiveness, and changed the design through new styles and fonts

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-$(function(){
-    $('.tlt').textillate({ in : {
-            effect: 'bounceIn',
-            shuffle: false,
-            sequence: true,
-        },
-    });
+$(function() {
+  $('.tlt').textillate({ in : {
+      effect: 'bounceIn',
+      shuffle: false,
+      sequence: true,
+    },
+  });
 
-    $('.tlt-about-p').textillate({ in : {
-            effect: 'bounceIn',
-            shuffle: false,
-            sequence: true,
-            delayScale: 0.2,
-            callback: function() {
- $('.tlt-contact').textillate({ in : {
+  $('.tlt-about-p').textillate({ in : {
+      effect: 'bounceIn',
+      shuffle: false,
+      sequence: true,
+      delayScale: 0.1,
+      callback: function() {
+        $('.tlt-contact').textillate({ in : {
             effect: 'flash',
             shuffle: false,
             sequence: true,
-        },
-        out: {
-            effect: 'shake',
-            shuffle: true,
+          },
+          out: {
+              effect: 'shake',
+              shuffle: true,
             },
-        loop: true
-    });            }
+            loop: true
+        });
+      }
 
-        }
-    });
+    }
+  });
 
-    $( "button" ).click(function() {
-        $('#pic').slideToggle( "slow" )
-    });
+  $("button").click(function() {
+    $('#pic').slideToggle("slow");
+    $('.marquee').fadeToggle();
+    $('header > span').css('padding', '10px')
+  });
 
-    $('.marquee').marquee();
+  $('.marquee').marquee();
 
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': false
+  })
 });
